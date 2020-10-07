e5a2d8ec967e337d1b7af4fac18ce6e7b8400d47
yubachiri <k.i.moso9532@gmail.com>
2020-10-07 18:24:16 +0900

    helloQuery.go整備

--- graphql/queries/helloQuery.go
+++ graphql/queries/helloQuery.go
@@ -1,8 +1,6 @@
 package queries
 
 import (
-	"fmt"
-
 	"github.com/graphql-go/graphql"
 )
 
@@ -16,17 +14,14 @@ var Hello graphql.Field = graphql.Field{
 
 // HelloWithArg query with argument "id"
 var HelloWithArg graphql.Field = graphql.Field{
-	Type:        graphql.String,
-	Description: "Print Arg",
+	Type: graphql.String,
 	Args: graphql.FieldConfigArgument{
 		"id": &graphql.ArgumentConfig{
 			Type: graphql.String,
 		},
 	},
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		fmt.Printf("%v\n", p)
 		idQuery, isOK := p.Args["id"].(string)
-		fmt.Printf(idQuery)
 		if isOK {
 			return idQuery, nil
 		}
